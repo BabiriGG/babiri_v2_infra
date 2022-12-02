b03fbb90991494843547ca68e7c16a4fd33c3778
kelvinkoon <kkoon@shaw.ca>
2022-12-01 21:40:39 -0500

    Initial commit

---
+++ test/babiri_v2_infra.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as BabiriV2Infra from '../lib/babiri_v2_infra-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/babiri_v2_infra-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new BabiriV2Infra.BabiriV2InfraStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
